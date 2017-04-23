b3f338f54a2929b513f2b96517b7532c3d231ef0
Bright Zheng <bzqzheng@gmail.com>
2017-04-23 14:46:01 -0400

    chord set for individual songs

--- Process_data_bright.py
+++ Process_data_bright.py
@@ -1,11 +1,12 @@
 
 # coding: utf-8
 
-# In[56]:
+# In[84]:
 
 import pandas
 pandas.set_option('display.max_rows',1000)
 pandas.set_option('display.max_columns',10000)
+import collections
 
 
 # In[57]:
@@ -35,6 +36,27 @@ for chords in m2016.chords:
         temp_str = str(cd_list[index] + ' ' + cd_list[index+1])
         chord_pair.append(temp_str)
 set(chord_pair)
+
+
+# In[110]:
+
+cd0 = m2016.chords[0]
+cd_pair = list()
+cd0_list = cd0.split(' ')
+for i in range(0, len(cd0_list)-1):
+    temp_str = str(cd0_list[i] + ' ' + cd0_list[i+1])
+    cd_pair.append(temp_str)
+cd_pair_set = set(cd_pair)
+
+
+# In[112]:
+
+cd_pair_set
+
+
+# In[113]:
+
+cd_pair
 
 
 # In[ ]:
